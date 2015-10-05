deecf104f0659b9eb11f355a10df537230f6a478
Nik <Nik@Nik-Labtop>
2015-10-04 21:19:23 -0600

    Fixxed formatting and misc errors

--- src/selectivereceiver/Receiver.java
+++ src/selectivereceiver/Receiver.java
@@ -16,41 +16,56 @@ import java.util.logging.Logger;
 
 public class Receiver {
 
+    int window;
     static int size = 128;
-    
+
     public static void main(String ARGS[]) {
         Receiver rex = new Receiver();
     }
 
     public Receiver() {
-        System.out.println("Got to receiver.");
         try {
+
             DatagramSocket receiverSocket = new DatagramSocket(9876);
             DatagramSocket acknowledgementSocket = new DatagramSocket(9879);
             byte[] rcvData = new byte[size];
             byte[] ackData = new byte[size];
             DatagramPacket rcvPkt = new DatagramPacket(rcvData, rcvData.length);
             receiverSocket.receive(rcvPkt); // should grab all the packet info and everything
+            System.out.println("Received packet");
             InetAddress IPAddress = rcvPkt.getAddress();
             int port = receiverSocket.getPort();
             DatagramPacket ackPkt;
             rcvData = rcvPkt.getData();
             int sequence = rcvData[0];
+            window = rcvData[1];
             boolean drop[] = new boolean[sequence];
             drop = setDrop(rcvData, drop);
             ackPkt = new DatagramPacket(rcvData, rcvData.length, IPAddress, 9879);
             acknowledgementSocket.send(ackPkt);
             int i = 0; // this value will track received packets
+            boolean[] windowTracker = new boolean[sequence];
+            for (int j = 0; j < sequence; j++) {
+                windowTracker[j] = false;
+            }
 
+            int windowFirst = 0;
             do {
                 receiverSocket.receive(rcvPkt);
                 rcvData = rcvPkt.getData();
                 int a = rcvData[0];
                 if (drop[a] == false) {
-                    ackData = rcvData;
-                    ackPkt = new DatagramPacket(rcvData, rcvData.length, IPAddress, 9879);
+                    ackData[0] = (byte) a;
+                    windowTracker[a] = true;
+                    if (windowFirst == a) {
+                        windowFirst++;
+                    }
+                    System.out.println("Packet " + a + " is received, send Ack" + a + ", window" + windowMaker(windowFirst, windowTracker));
+
+                    ackPkt = new DatagramPacket(ackData, ackData.length, IPAddress, 9878);
                     acknowledgementSocket.send(ackPkt);
                     i++;
+
                 } else {
                     drop[a] = false;
                 }
@@ -61,18 +76,40 @@ public class Receiver {
         }
     }
 
+    public String windowMaker(int windowfirst, boolean[] windowTracker) {
+        String message = "[";
+        for (int i = 0; i < window; i++) {
+            
+            if (windowfirst + i - 1 < windowTracker.length) {
+                if (windowTracker[windowfirst + i]) {
+                    message = message + "" + (windowfirst + i) + "#";
+                } else {
+                    message = message + "" + (windowfirst + i) + "";
+                }
+
+            } else {
+                message = message + "-";
+            }
+
+            if (i != window - 1) {
+                message = message + ",";
+            } else {
+                message = message + "]";
+            }
+        }
+            return message;
+        
+    }
+
     public boolean[] setDrop(byte[] in_byte, boolean[] in_bool) {
 
-        for (int i = 1; i < in_byte.length; i++) {
+        for (int i = 2; i < size; i++) {
             int value = in_byte[i];
             if (value > 0) {
                 in_bool[value] = true;
             }
-
         }
-
         return in_bool;
     }
-
     //public void 
 }
